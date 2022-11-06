ccd07bb46fc7794430276428a58349d761149ead
aky03 <61700355+aky03@users.noreply.github.com>
2022-11-06 18:56:16 +0530

    Update index.js

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'CoMeds',
+        folder: 'Medify',
         allowedFormats: ['jpeg', 'png', 'jpg']
     }
 });
